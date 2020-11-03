a50f9ddc967a712d74a1fa4c162254640ab12d9d
Ana <ee.anap@gmail.com>
2020-11-03 10:10:36 -0300

    feat(django): add rest framework

--- my_web_django_project/my_app/views.py
+++ my_web_django_project/my_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK
+from rest_framework.viewsets import ViewSet
 
-# Create your views here.
+
+class MyViewSet(ViewSet):
+    def list(self, request):
+        """
+        GET /base/
+
+        """
+        return Response(status=HTTP_200_OK, data='Hello world')
